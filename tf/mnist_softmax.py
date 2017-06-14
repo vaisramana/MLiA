a9a0986f485e5381afdd55054cced2e990cbc91d
vaisramana <zhangshuo.chn@gmail.com>
2017-06-14 15:11:55 +0800

    sigmoid_cross_entropy_with_logits is slower than softmax_cross_entropy_with_logits

--- tf/mnist_softmax.py
+++ tf/mnist_softmax.py
@@ -39,20 +39,29 @@ def main(_):
   # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
   # outputs of 'y', and then average across the batch.
   cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+      #tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+      tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=y))
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
   sess = tf.InteractiveSession()
   tf.global_variables_initializer().run()
+  
+  # forward propagation
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  
   # Train
-  for _ in range(1000):
+  for i in range(2000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    if i%10 == 0:
+        print("tainning accuracy: ",sess.run(accuracy, feed_dict={x: mnist.train.images,
+                                      y_: mnist.train.labels}))
 
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+  print("testing accuracy: ",sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
 if __name__ == '__main__':
